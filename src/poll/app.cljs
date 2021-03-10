04c08b4a09b9fa31724b7b15bcdf82f09bd8e73e
Rameez Khan <rameezkhan.sa@gmail.com>
2021-03-10 07:48:54 +0200

    Add app sources

---
+++ src/poll/app.cljs
@@ -0,0 +1,25 @@
+(ns poll.app
+  (:require [reagent.dom :as rd]
+            [re-frame.core :as rf]))
+
+(def v 1)
+
+(defn- version []
+  [:span {:style {:position "fixed" :right "1rem" :bottom "1rem"}}(str "v" v)])
+
+(defn app []
+  [:div.section
+   {:style {:height "100vh"}}
+   [:div.container
+    [version]]])
+
+(defn mount-reagent []
+  (rd/render app (js/document.getElementById "app")))
+
+(defn ^:export run []
+  ;; (rf/dispatch-sync [::state/initialize])
+  (mount-reagent))
+
+(defn ^:dev/after-load shaddow-start [] (mount-reagent))
+(defn ^:dev/before-load shaddow-stop [])
+
